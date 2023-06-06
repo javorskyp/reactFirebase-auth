fe8b350dd6703701ad2a39c6c61b18ea541a6595
Pawel Jaworski Software <jpsoftware@MacBook-Pro-Pawel.local>
2023-06-06 21:05:18 +0200

    form styles

---
+++ pages/auth/login.js
@@ -0,0 +1,27 @@
+import {FcGoogle} from 'react-icons/fc';
+import { AiFillFacebook } from 'react-icons/ai'
+
+export default function Login() {
+    return (
+    <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
+      <h2 className="text-3xl font-medium">Join today</h2>
+      <div className="py-4">
+        <h3 className="py-4">Sign in with one of the providers</h3>
+        <div className="flex flex-col gap-4">
+          <button
+            className="text-white bg-gray-700 p-4 w-full font-medium rounded-lg flex align-middle gap-2 "
+          >
+            <FcGoogle className="text-2xl" />
+            Sign in with Google
+          </button>
+          <button
+            className="text-white bg-gray-700 p-4 w-full font-medium rounded-lg flex align-middle gap-2 "
+          >
+            <AiFillFacebook className="text-2xl text-blue-300" />
+            Sign in with Facebook
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
