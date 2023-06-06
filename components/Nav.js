f85782648d73e3a5be3437278a3620ff12f0baba
Pawel Jaworski Software <jpsoftware@MacBook-Pro-Pawel.local>
2023-06-06 22:08:50 +0200

    login photo dashboard

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,30 @@
 import Link from "next/link";
+import {useAuthState} from 'react-firebase-hooks/auth'
+import {auth} from '../utils/firebase'
 
 export default function Nav () {
+
+    const [user, loading] = useAuthState(auth);
+
     return (
         <nav className="mx-14 flex justify-between items-center py-10">
             <Link href={"/"}>Logo</Link>
             <ul>
+            {!user && (
             <Link href="/auth/login" className="py-2 px-4 text-lg bg-teal-500 text-white rounded-lg font-medium ml-8">
                 Join now
           </Link>
+    )}
+            {user && (
+                <div>
+                    <Link href={'/dashboard'}>
+                        <img src={user.photoURL} 
+                             alt="avatar"
+                             referrerPolicy="no-refferrer"
+                             className="w-12 rounded-full" />
+                    </Link>
+                </div>
+            )}
             </ul>
         </nav>
     );
--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,14 +1,33 @@
 import {FcGoogle} from 'react-icons/fc';
 import { AiFillFacebook } from 'react-icons/ai'
+import {
+    signInWithPopup,
+    GoogleAuthProvider,
+    FacebookAuthProvider,
+    updateProfile,
+  } from "firebase/auth";
+  import { auth } from "../../utils/firebase";
 
 export default function Login() {
+
+    const googleProvider = new GoogleAuthProvider();
+    const GoogleLogin = async () => {
+        try {
+          const result = await signInWithPopup(auth, googleProvider);
+          console.log(result.user);
+          route.push("/dashboard");
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
     return (
     <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
       <h2 className="text-3xl font-medium">Join today</h2>
       <div className="py-4">
         <h3 className="py-4">Sign in with one of the providers</h3>
         <div className="flex flex-col gap-4">
-          <button
+          <button onClick={GoogleLogin}
             className="text-white bg-gray-700 p-4 w-full font-medium rounded-lg flex align-middle gap-2 "
           >
             <FcGoogle className="text-2xl" />
